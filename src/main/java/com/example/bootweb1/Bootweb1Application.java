82ea9b131c376c84e197de8272aa52e0e940ccea
JaeMin Kim <brodieroy@outlook.kr>
2018-01-11 19:34:05 +0900

    init bootest

---
+++ src/main/java/com/example/bootweb1/Bootweb1Application.java
@@ -0,0 +1,18 @@
+package com.example.bootweb1;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Bootweb1Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Bootweb1Application.class);
+    }//test
+}
